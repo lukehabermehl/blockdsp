7f7f1334fecb4ba134245a7c8f34b4b7cf63e6e1
Luke Habermehl <luke.habermehl@gmail.com>
2016-06-18 11:12:23 -0400

    Export blockdsp.h to include all public headers. Change target include path for other headers to /usr/local/include/blockdsp

---
+++ blockdsp.h
@@ -0,0 +1,27 @@
+//
+//  blockdsp.h
+//  libblockdsp
+//
+//  Created by Luke on 6/18/16.
+//  Copyright © 2016 Luke Habermehl. All rights reserved.
+//
+
+/*
+ Includes all public headers for libblockdsp
+*/
+
+#ifndef blockdsp_h
+#define blockdsp_h
+
+#include <blockdsp/APUParameter.hpp>
+#include <blockdsp/AudioFile.hpp>
+#include <blockdsp/AudioManager.hpp>
+#include <blockdsp/AudioProcessingUnit.hpp>
+#include <blockdsp/BDAPULoader.hpp>
+#include <blockdsp/BDCodeBuilder.hpp>
+#include <blockdsp/BlockDSPNode.hpp>
+#include <blockdsp/BlockDSPNumber.hpp>
+#include <blockdsp/BlockDSPParameter.hpp>
+#include <blockdsp/BlockDSPSystem.hpp>
+
+#endif /* blockdsp_h */
