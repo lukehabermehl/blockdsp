502b356f06fd52940fc052f871c9083c17beaea4
Luke Habermehl <luke.habermehl@gmail.com>
2016-09-29 14:42:46 -0400

    Add some logging to apuloader

--- Code Generation/bdsp_apuloader.cpp
+++ Code Generation/bdsp_apuloader.cpp
@@ -6,9 +6,11 @@
 #include "bdsp_apuloader.hpp"
 #include "bdsp_apuloader_private.hpp"
 #include "bdsp_system.hpp"
+#include "bdsp_logger.hpp"
 #include <dlfcn.h>
 
 #define kAPUFactoryFuncName "AudioProcessingUnitFactoryCreate"
+static const char *kApuLoaderPrefix = "[BDAPULoader]";
 
 BDAPULoader::BDAPULoader()
 {
@@ -28,12 +30,17 @@ BDAPULoader::~BDAPULoader()
 
 AudioProcessingUnit * BDAPULoader::loadAPU(const char *filepath)
 {
+    BDLogFormat(kApuLoaderPrefix, "Load APU at file path: %s", filepath);
     void *dllHandle = dlopen(filepath, RTLD_NOW);
     if (!dllHandle)
     {
         _pimpl->error = BDAPULoaderErrorLibraryLoadFailed;
+        BDLog(kApuLoaderPrefix, "dlopen() call failed");
+
         return NULL;
     }
+
+    BDLog(kApuLoaderPrefix, "dylib loaded!");
     
     dlerror(); //clear dlerror
     
@@ -41,12 +48,23 @@ AudioProcessingUnit * BDAPULoader::loadAPU(const char *filepath)
     if (dlerror() != NULL)
     {
         _pimpl->error = BDAPULoaderErrorSymbolNotFound;
+        BDLog(kApuLoaderPrefix, "Failed to find APU factory function in dylib");
         return NULL;
     }
+
+    BDLog(kApuLoaderPrefix, "Found APU factory function");
     
     _pimpl->handle = dllHandle;
     
     AudioProcessingUnit *apu = apuFactoryFn();
+    if (apu)
+    {
+        BDLogFormat(kApuLoaderPrefix, "APU: %s loaded successfully!", apu->getName());
+    }
+    else
+    {
+        BDLog(kApuLoaderPrefix, "APU factory function returned NULL");
+    }
     
     return apu;
 }
