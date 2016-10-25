4dc1249abd3c184f523f5ec358d2a2a2aee17de0
Luke Habermehl <luke.habermehl@gmail.com>
2016-10-24 20:30:25 -0400

    Add builtin Gain Effect

---
+++ BuiltIns/bdsp_builtin_gain.cpp
@@ -0,0 +1,45 @@
+//
+//  bdsp_builtin_gain.cpp
+//  libblockdsp
+//
+//  Created by Luke on 10/24/16.
+//  Copyright © 2016 Luke Habermehl. All rights reserved.
+//
+
+#include "bdsp_builtin_gain.hpp"
+
+BDSPGainEffect::BDSPGainEffect() : AudioProcessingUnit()
+{
+    gainParameter = new APUParameter("Gain", APUNUM_FLOAT);
+    gainParameter->setMaxValue(APUNUM_FLOAT(2));
+    gainParameter->setMinValue(APUNUM_FLOAT(0));
+    gainParameter->setValue(APUNUM_FLOAT(1));
+
+    addParameter(gainParameter);
+}
+
+void BDSPGainEffect::setupInitialState()
+{
+}
+
+void BDSPGainEffect::processAudio(float *inputBuffer, float *outputBuffer, int numInputChannels, int numOutputChannels)
+{
+    float fGain = gainParameter->getCurrentValue().floatValue();
+    outputBuffer[0] = inputBuffer[0] * fGain;
+    if (numOutputChannels == 2)
+    {
+        if (numInputChannels == 1)
+        {
+            outputBuffer[1] = outputBuffer[0];
+        }
+        else
+        {
+            outputBuffer[1] = inputBuffer[1] * fGain;
+        }
+    }
+}
+
+const char * BDSPGainEffect::getName()
+{
+    return "Gain Effect";
+}
---
+++ BuiltIns/bdsp_builtin_gain.hpp
@@ -0,0 +1,26 @@
+//
+//  bdsp_builtin_gain.hpp
+//  libblockdsp
+//
+//  Created by Luke on 10/24/16.
+//  Copyright © 2016 Luke Habermehl. All rights reserved.
+//
+
+#ifndef bdsp_builtin_gain_hpp
+#define bdsp_builtin_gain_hpp
+
+#include "blockdsp.h"
+
+class BDSPGainEffect : public AudioProcessingUnit
+{
+public:
+    BDSPGainEffect();
+    virtual void setupInitialState();
+    virtual const char * getName();
+    virtual void processAudio(float *inputBuffer, float *outputBuffer, int numInputChannels, int numOutputChannels);
+
+protected:
+    APUParameter *gainParameter;
+};
+
+#endif /* bdsp_builtin_gain_hpp */
